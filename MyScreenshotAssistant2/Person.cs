3f857569a3b4c283b33a727482a49a8c67367d35
come25136 <come25136@gmail.com>
2016-09-02 11:24:48 +0900

    [add] UIやり取り用クラス

---
+++ MyScreenshotAssistant2/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace MyScreenshotAssistant2
+{
+    class Person
+    {
+        public BitmapImage AccountIcon { get; set; }
+        public string AccountName { get; set; }
+    }
+}

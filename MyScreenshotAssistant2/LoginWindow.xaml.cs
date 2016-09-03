65d53cf36c7e750a9c2224f88a192b40b21823d3
come25136 <come25136@gmail.com>
2016-09-03 18:53:51 +0900

    [clean] 要らないusingを消した

--- MyScreenshotAssistant2/LoginWindow.xaml.cs
+++ MyScreenshotAssistant2/LoginWindow.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Data;
+﻿using System.Data;
 using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Forms;
 
 namespace MyScreenshotAssistant2
 {

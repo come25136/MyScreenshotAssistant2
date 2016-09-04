02b1a32bad1111dc799d6981f2b7f480d9a1225a
come25136 <come25136@gmail.com>
2016-09-04 22:02:31 +0900

    [add] アップデート通知機能を実装

---
+++ MyScreenshotAssistant2/UpdateWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace MyScreenshotAssistant2
+{
+    /// <summary>
+    /// UpdateWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UpdateWindow : Window
+    {
+        public UpdateWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new update()
+            {
+                new_version = Update.msajson.version,
+                current_version = MainWindow.version
+            };
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(Update.msajson.dlurl);
+        }
+    }
+}
